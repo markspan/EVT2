d7486616b1f9094744e29c910bfef61c04fa40f9
markspan <m.m.span@gmail.com>
2016-11-29 09:07:29 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -11,20 +11,18 @@ from setuptools import setup
 setup(
 	# Some general metadata. By convention, a plugin is named:
 	# opensesame-plugin-[plugin name]
-	name='opensesame-plugin-example_plugin',
+	name='opensesame-plugin-Pulse_EVT2',
 	version='0.0.1',
-	description='Example plugin for OpenSesame',
-	author='Your Name',
-	author_email='yourname@domain.com',
+	description='Send markers through an EVT2 (RUG USB interface)',
+	author='Mark Span',
+	author_email='m.m.span@rug.nl',
 	url='https://github.com/your/repository',
 	# Classifiers used by PyPi if you upload the plugin there
 	classifiers=[
 		'Intended Audience :: Science/Research',
 		'Topic :: Scientific/Engineering',
-		'Environment :: MacOS X',
 		'Environment :: Win32 (MS Windows)',
-		'Environment :: X11 Applications',
-		'License :: OSI Approved :: Apache Software License',
+		'License :: OSI Approved :: GNU Public Licence v3',
 		'Programming Language :: Python :: 2',
 		'Programming Language :: Python :: 3',
 	],
@@ -34,15 +32,20 @@ setup(
 	# module.
 	data_files=[
 		# First the target folder.
-		('share/opensesame_plugins/example_plugin',
+		('share/opensesame_plugins/Pulse_EVT2',
 		# Then a list of files that are copied into the target folder. Make sure
 		# that these files are also included by MANIFEST.in!
 		[
-			'opensesame_plugins/example_plugin/example_plugin.md',
-			'opensesame_plugins/example_plugin/example_plugin.png',
-			'opensesame_plugins/example_plugin/example_plugin_large.png',
-			'opensesame_plugins/example_plugin/example_plugin.py',
-			'opensesame_plugins/example_plugin/info.yaml',
+			'opensesame_plugins/Pulse_EVT2/Pulse_EVT2.md',
+			'opensesame_plugins/Pulse_EVT2/Pulse_EVT2.png',
+			'opensesame_plugins/Pulse_EVT2/Pulse_EVT2_large.png',
+			'opensesame_plugins/Pulse_EVT2/Pulse_EVT2.py',
+			'opensesame_plugins/Pulse_EVT2/libevt.py',
+			'opensesame_plugins/Pulse_EVT2/EventExchanger.dll',
+			'opensesame_plugins/Pulse_EVT2/HidSharp.dll',
+			'opensesame_plugins/Pulse_EVT2/HidSharp.DeviceHelpers.dll',
+			'opensesame_plugins/Pulse_EVT2/info.yaml',
+			'opensesame_plugins/Pulse_EVT2/Example.osexp',
 			]
 		)]
 	)
